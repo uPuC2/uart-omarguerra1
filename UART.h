ec0d3bcdb83c8f705c9d5785411f51c09a1c25b9
omarguerra1 <99432120+omarguerra1@users.noreply.github.com>
2025-04-11 18:39:54 -0700

    Final UART.h

--- UART.h
+++ UART.h
@@ -1,25 +1,16 @@
-// Prototypes
-// Initialization
-UART_Ini(uint8_t com, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop)
-
-// Send
-void UART_puts(uint8_t com, char *str);
-void UART_putchar(uint8_t com, char data);
-
-// Received
-uint8_t UART_available(uint8_t com);
-char UART_getchar(uint8_t com );
-void UART_gets(uint8_t com, char *str);
-
-// Escape sequences
-UART_clrscr( uint8_t com );
-UART_setColor(uint8_t com, uint8_t color);
-UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
-
-#define YELLOW  0 // Fixme 
-#define GREEN   0 // Fixme 
-#define BLUE    0 // Fixme 
-
-// Utils
-void itoa(uint16_t number, char* str, uint8_t base) 
-uint16_t atoi(char *str)
+#ifndef _UART
+#define _UART
+#include <avr/io.h>
+void UART_Ini(uint8_t com, uint16_t baudrate, uint8_t size, uint8_t parity, uint8_t stop);//Inicializar el UART
+char UART_getchar(uint8_t com); //Funcion para obtener el caracter
+void UART_gets(char *str, uint8_t com); //FUNCION PARA OBTENER TODOS LOS DATOS DE GETCHAR EN UNA CADENA
+void UART_putchar(char dato, uint8_t com); //Funcion para mostrar el caracter al teclear 
+void UART_puts(char *str, uint8_t com); //Imprime la cadena
+uint8_t UART0_available(void);//FUNCION PARA SABER SI LA CADENA ESTA LLENA O NO
+void ITOA(char *str, uint16_t input, uint8_t base); //convierte numeros a cadena
+uint16_t ATOI(char *str); //convierte cadena a numero
+void gotoxy(uint8_t com, uint8_t x, uint8_t y); //posiciona el texto en una posicion de la consola
+void setColor(uint16_t color);//FUNCION DE SECUENCIA DE ESCAPE PARA CAMBIAR COLOR DE TEXTO
+void clrscr(uint8_t com); //limpia la pantalla
+enum color{NEGRO = 30, ROJO, VERDE, AMARILLO, AZUL, MAGENTA, CIAN, BLANCO};
+#endif
